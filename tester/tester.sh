3143e59eb65212d4b64cc4c76076516735070b4f
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2025-07-07 14:56:51 +0000

    brand new tester, better stronger, easier

--- tester/tester.sh
+++ tester/tester.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-# Couleurs pour l'affichage
-GREEN="\033[32m"
-RED="\033[31m"
-RESET="\033[0m"
+mkdir -p results
+LOGFILE_DIE="results/logtest_die"
+LOGFILE_ALIVE="results/logtest_alive"
 
-echo -e "\n=== Tests des cas où le philosophe doit mourir ===\n"
+> "$LOGFILE_DIE"
+> "$LOGFILE_ALIVE"
 
-# Exécute tous les tests dans le dossier should_die
-for test in eval_sheet/*.sh; do
-    echo -n "Test $(basename $test): "
-    bash "$test"
-done
+function run_tests() {
+	local file="$1"
+	local log="$2"
+
+	echo "Execution of the tests in $file"
+	while IFS= read -r line || [ -n "$line" ]; do
+		echo "testing \"$line\" :"
+		echo "Exécution : $line" >> "$log"
+		
+		# Remplace seulement './philo' par '../philo' sans toucher à timeout ou autre
+		cmd=$(echo "$line" | sed 's|^\(.*\)\./philo|\1../philo|')
+
+		# Exécution et nettoyage des couleurs ANSI
+		eval "$cmd" | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' >> "$log" 2>&1
+
+		echo "complete"
+		echo "-----------------------------" >> "$log"
+		echo "---------END OF TEST---------" >> "$log"
+		echo "-----------------------------" >> "$log"
+	done < "$file"
+}
+
+run_tests die_tests "$LOGFILE_DIE"
+run_tests alive_tests "$LOGFILE_ALIVE"
